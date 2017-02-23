eca35b4a00022aad97ab94405f9570ce4cda2079
Andy <andy9775@gmail.com>
2017-02-23 17:46:33 -0500

    chapter 2 - basic routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,29 @@
 require 'rulers/version'
 require 'rulers/array' # make convenience methods/classes available to apps
+require 'rulers/routing'
 
 module Rulers
   class Application
-    def call(_env)
-      `echo debug > debug.txt` # gets called per request.
-      [200, { 'Content-Type' => 'text/html' }, ['Hello from rulers']]
+    def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        # ignore requests for favicon
+        return [404, { 'Content-Type' => 'text/html' }, []]
+      end
+      # fetch the controller and action to use from the URL
+      # e.g. /quotes/q_quote (quotes controller a_quote action (method))
+      klass, action = get_controller_and_action env
+      controller = klass.new env
+      text = controller.send action
+
+      [200, { 'Content-Type' => 'text/html' }, [text]]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
